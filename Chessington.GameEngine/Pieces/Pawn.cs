716edc0311fe4efba4b908dea54db810f79747f6
Glenn Clarke <Glenn.Clarke@softwire.com>
2022-07-11 14:09:35 +0100

    pawns can take opposing pieces

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -16,15 +16,46 @@ namespace Chessington.GameEngine.Pieces
             Square currentloc = board.FindPiece(this);
             List<Square> availableMoves = new List<Square>();
             int playerMod = (Player == Player.White) ? -1 : 1; //Modifies the move based on the owner
-            RelativeMove(currentloc, availableMoves, 0, 1, board, playerMod, canTake:false);
-            
-            if (!moved)// && availableMoves.Count > 0)
+            RelativeMove(currentloc, availableMoves, 0, 1, board, playerMod, canTake: false);
+
+            if (!moved) // && availableMoves.Count > 0)
             {
-                RelativeMove(currentloc, availableMoves, 0, 2, board, playerMod, canTake:false, ignorePath:false);
+                RelativeMove(currentloc, availableMoves, 0, 2, board, playerMod, canTake: false, ignorePath: false);
             }
+
+            int rowTarget = currentloc.Row + (1 * playerMod);
+            int colTarget = currentloc.Col + (1 * playerMod);
+            Square s = Square.At(rowTarget, colTarget); // Gets the appropriate square
             
+            //Diagonal only if piece to take
+            if (!(rowTarget is < 0 or > GameSettings.BoardSize - 1 || colTarget is < 0 or > GameSettings.BoardSize - 1)) // if invalid index
+            {
+                if (s != currentloc) // if current index
+                {
+                    if (board.GetPiece(s) != null && board.GetPiece(s).Player != Player)
+                    {
+                        if (!availableMoves.Contains(s)) //if it is not already in the list
+                            availableMoves.Add(s); // Adds move to list
+                    }
+                }
+            }
+            rowTarget = currentloc.Row + (1 * playerMod);
+            colTarget = currentloc.Col + (-1 * playerMod);
+            s = Square.At(rowTarget, colTarget); // Gets the appropriate square
+            if (!(rowTarget is < 0 or > GameSettings.BoardSize - 1 || colTarget is < 0 or > GameSettings.BoardSize - 1)) // if invalid index
+            {
+                if (s != currentloc) // if current index
+                {
+                    if (board.GetPiece(s) != null && board.GetPiece(s).Player != Player)
+                    {
+                        if (!availableMoves.Contains(s)) //if it is not already in the list
+                            availableMoves.Add(s); // Adds move to list
+                    }
+                }
+            }
             return availableMoves;
         }
+
 
         public override void MoveTo(Board board, Square newSquare) //Overrides base method and just adds change to moved property
         {
