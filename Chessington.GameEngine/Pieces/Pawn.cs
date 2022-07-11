36a8894e57b97879275e5a2af074b5da185b91fc
Glenn Clarke <Glenn.Clarke@softwire.com>
2022-07-11 09:43:06 +0100

    Pawn movement passes tests

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -10,7 +10,12 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            Square currentloc = board.FindPiece(this);
+            List<Square> availableMoves = new List<Square>();
+            int playerMod = (Player == Player.White) ? -1 : 1; //Modifies the move based on the owner
+            Square s = Square.At(currentloc.Row + 1*playerMod, currentloc.Col); // Gets the appropriate square
+            availableMoves.Add(s); // Adds move to list
+            return availableMoves;
         }
     }
 }
