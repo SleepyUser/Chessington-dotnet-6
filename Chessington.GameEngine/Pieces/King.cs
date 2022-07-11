b49d014a16df0a75a200da806f8c3cbe55e85814
Glenn Clarke <Glenn.Clarke@softwire.com>
2022-07-11 10:50:12 +0100

    King tests pass

--- Chessington.GameEngine/Pieces/King.cs
+++ Chessington.GameEngine/Pieces/King.cs
@@ -10,7 +10,20 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            Square currentloc = board.FindPiece(this);
+            List<Square> availableMoves = new List<Square>();
+            int playerMod = (Player == Player.White) ? -1 : 1; //Modifies the move based on the owner
+            
+            RelativeMove(currentloc, playerMod, availableMoves, 1, -1);
+            RelativeMove(currentloc, playerMod, availableMoves, 1, 1);
+            RelativeMove(currentloc, playerMod, availableMoves, -1, -1);
+            RelativeMove(currentloc, playerMod, availableMoves, -1, 1);
+            RelativeMove(currentloc, playerMod, availableMoves, 0, -1);
+            RelativeMove(currentloc, playerMod, availableMoves, 0, 1);
+            RelativeMove(currentloc, playerMod, availableMoves, 1, 0);
+            RelativeMove(currentloc, playerMod, availableMoves, -1, 0);
+
+            return availableMoves;
         }
     }
 }
