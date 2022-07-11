38774380a12d1efaab35cf46c1957626e91db909
Glenn Clarke <Glenn.Clarke@softwire.com>
2022-07-11 10:41:26 +0100

    Queen can move both diagonally and laterally

--- Chessington.GameEngine/Pieces/Queen.cs
+++ Chessington.GameEngine/Pieces/Queen.cs
@@ -10,7 +10,21 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            Square currentLoc = board.FindPiece(this);
+            List<Square> availableMoves = new List<Square>();
+            int playerMod = (Player == Player.White) ? -1 : 1; //Modifies the move based on the owner
+            for (int i = 0; i < GameSettings.BoardSize; i++)
+            {
+                RelativeMove(currentLoc, playerMod, availableMoves, i, -i);
+                RelativeMove(currentLoc, playerMod, availableMoves, -i, i);
+                RelativeMove(currentLoc, playerMod, availableMoves, -i, -i);
+                RelativeMove(currentLoc, playerMod, availableMoves, i, i);
+                AbsoluteMove(currentLoc, availableMoves, i, currentLoc.Row);
+                AbsoluteMove(currentLoc, availableMoves, currentLoc.Col, i );
+            }
+
+            availableMoves = availableMoves.Distinct().ToList();
+            return availableMoves;
         }
     }
 }
