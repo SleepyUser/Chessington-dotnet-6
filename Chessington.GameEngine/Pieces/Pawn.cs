4aad2d6b313cec183d50483ef4846891f76b8db6
Glenn Clarke <Glenn.Clarke@softwire.com>
2022-07-11 09:59:36 +0100

    Pawns can now double move as first move

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Windows.Shell;
 
 namespace Chessington.GameEngine.Pieces
 {
@@ -8,14 +9,27 @@ namespace Chessington.GameEngine.Pieces
         public Pawn(Player player) 
             : base(player) { }
 
+        private bool moved = false;
+
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
             Square currentloc = board.FindPiece(this);
             List<Square> availableMoves = new List<Square>();
             int playerMod = (Player == Player.White) ? -1 : 1; //Modifies the move based on the owner
-            Square s = Square.At(currentloc.Row + 1*playerMod, currentloc.Col); // Gets the appropriate square
-            availableMoves.Add(s); // Adds move to list
+            StandardMove(currentloc, playerMod, availableMoves, 0, 1);
+
+            if (!moved)
+            {
+                StandardMove(currentloc, playerMod, availableMoves, 0, 2);
+            }
+            
             return availableMoves;
+        }
+
+        public override void MoveTo(Board board, Square newSquare) //Overrides base method and just adds change to moved property
+        {
+            base.MoveTo(board, newSquare);
+            moved = true;
         }
     }
 }
--- Chessington.GameEngine/Pieces/Piece.cs
+++ Chessington.GameEngine/Pieces/Piece.cs
@@ -13,10 +13,16 @@ namespace Chessington.GameEngine.Pieces
 
         public abstract IEnumerable<Square> GetAvailableMoves(Board board);
 
-        public void MoveTo(Board board, Square newSquare)
+        public virtual void MoveTo(Board board, Square newSquare)
         {
             var currentSquare = board.FindPiece(this);
             board.MovePiece(currentSquare, newSquare);
+        }
+        
+        protected static void StandardMove(Square currentLoc, int playerMod, List<Square> availableMoves, int colMod, int rowMod)
+        {
+            Square s = Square.At(currentLoc.Row + rowMod * playerMod, currentLoc.Col + colMod * playerMod); // Gets the appropriate square
+            availableMoves.Add(s); // Adds move to list
         }
     }
 }
